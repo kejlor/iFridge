9add4afac0f6f0009295f2f2f15eb41505062dc1
kejlor <woj450@gmail.com>
2022-02-10 15:09:57 +0100

    Initial Commit

---
+++ iFridge/iFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFridgeApp.swift
+//  iFridge
+//
+//  Created by Bartosz Wojtkowiak on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct iFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
